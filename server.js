d3a90722b5f26579a7b7e904389d8f099fbc89be
jakyungjung <newmade01@naver.com>
2024-06-12 21:21:57 +0900

    yaml파일 deployment

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require("http");
 http
   .createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World");
+    response.end("Hello kubernets!!!");
   })
-  .listen(8081);
+  .listen(8080);
 
-console.log("Server running at http://127.0.0.1:8081/");
+console.log("Server running at http://127.0.0.1:8080/");
