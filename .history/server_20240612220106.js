9f7798d7dfcea1689b58cdaa46365a2794985146
jakyungjung <newmade01@naver.com>
2024-06-12 22:18:49 +0900

    웹서버 kubernets

--- .history/server_20240612220106.js
+++ .history/server_20240612220106.js
@@ -4,8 +4,8 @@ var host = os.hostname();
 http
   .createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("server running at  " + host);
+    response.end("server running at" + host);
   })
-  .listen(8083);
+  .listen(8080);
 
-console.log("Server running at http://127.0.0.1:8083/");
+console.log("Server running at http://127.0.0.1:8080/");
--- .history/server_20240612220126.js
+++ .history/server_20240612220126.js
@@ -4,7 +4,7 @@ var host = os.hostname();
 http
   .createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("server running at  " + host);
+    response.end("server running at" + host);
   })
   .listen(8083);
 
